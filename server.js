1dcddca2194d139b56194486aa1a8ec85bddce8e
gauravgulia26 <grvgulia007@gmail.com>
2023-02-23 00:09:44 +0530

    added ejs and maths to calculate the even odd

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const parser = require("body-parser");
 const path = require("path");
 const app = express();
+const name = require("ejs");
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.listen(3000, (req, res) => {
   console.log("hey i am working");
 });
 
 app.get("/", (req, res) => {
-  res.sendFile("public/html/index.html", { root: __dirname });
+  res.render("app", { result: "Result" });
+});
+
+app.post("/", (req, res) => {
+  let nmbr = req.body.entered_number;
+  if (nmbr % 2 === 0) {
+    res.render("app", { result: "Even" });
+  } else {
+    res.render("app", { result: "Odd" });
+  }
 });
